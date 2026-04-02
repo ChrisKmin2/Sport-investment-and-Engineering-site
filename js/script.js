ec980f3343d88b22b364f9aa367e048dbd77ee4d
ChrisKmin2 <106134198+ChrisKmin2@users.noreply.github.com>
2026-04-02 06:26:20 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const button= document.getElementById("mainButton");
+button.addEventListener("click", function(event){
+event.preventDefault();
+alert("You are now entering the future of sports and Engineering");
+window.location.href= "project.html"
+});
